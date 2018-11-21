18b9c590ad57b87f2b5be46a12844f3aeac342a9
Kaspar Pflugshaupt <kpflugshaupt@gmail.com>
2018-11-21 18:22:44 +0100

    init

---
+++ Alert Demo.js
@@ -0,0 +1,11 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: red; icon-glyph: exclamation-triangle;
+// Test script
+
+alert = new Alert();
+
+alert.addAction("Hallo Welt!");
+button = alert.presentAlert();
+
+// console.log(button.toString());
